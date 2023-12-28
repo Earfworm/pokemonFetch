40541770ffd33004da5e7b8c1d0b08e56974d450
earfworm <deshawnreid2021@gmail.com>
2023-12-28 01:31:10 -0500

    reading docs going to import data on pokemonQReq

--- src/components/PokemonQueryRequest.tsx
+++ src/components/PokemonQueryRequest.tsx
@@ -6,7 +6,7 @@ import { Pokemon } from "../models/Pokemon";
 const [allPokemonInfo, setAllPokemonInfo] = useState<Pokemon>();
 
 useEffect(() => {
-  getPokemonInfo().then((data) => {});
+  getPokemonInfo().then((data) => console.log(data));
 }, []);
 const PokemonQueryRequest = () => {
   return <div className="PokemonQueryRequest">PokemonQueryRequest works</div>;
